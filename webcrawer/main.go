a86a8a67348707f76968232a38dce749fc33829f
moris <moris.zhang@gmail.com>
2017-03-03 10:06:59 +0800

    sync changed

--- webcrawer/main.go
+++ webcrawer/main.go
@@ -9,6 +9,7 @@ import (
 	"os"
 	_ "runtime"
 	_ "strconv"
+	"strings"
 	_ "time"
 
 	"github.com/ian-kent/go-log/log"
@@ -17,31 +18,31 @@ import (
 func main() {
 	// Pass a log message and arguments directly
 	log.Debug("Example log message: %s", "example arg")
-	   
-	inputFile, inputError := os.Open("input.dat")
-    
-	if inputError != nil {
-        fmt.Printf("An error occurred on opening the inputfile\n" +
-            "Does the file exist?\n" +
-            "Have you got acces to it?\n")
-        return // exit the function on error
-    }
-    defer inputFile.Close()
 
-    inputReader := bufio.NewReader(inputFile)
-	i:=0
+	inputFile, inputError := os.Open("input.dat")
+
+	if inputError != nil {
+		fmt.Printf("An error occurred on opening the inputfile\n" +
+			"Does the file exist?\n" +
+			"Have you got acces to it?\n")
+		return // exit the function on error
+	}
+	defer inputFile.Close()
+
+	inputReader := bufio.NewReader(inputFile)
+	i := 0
 	var inputString []string
 	//var readerError error
 
-    for {
-        str, readerError := inputReader.ReadString('\n')
-        if readerError == io.EOF {
-            break
-        }
-		inputString = append(inputString,str)	
-        fmt.Printf("The input was:%d %s", i, inputString[i])
+	for {
+		str, readerError := inputReader.ReadString('\n')
+		if readerError == io.EOF {
+			break
+		}
+		inputString = append(inputString, str)
+		fmt.Printf("The input was:%d %s", i, inputString[i])
 		i++
-    }
+	}
 	/*var inputString [3]string
 	inputString[0]="http://www.google.com/robots.txt"
 	inputString[1]="http://www.google.com/robots.txt"
@@ -52,24 +53,26 @@ func main() {
 	//log.Debug(func(i ...interface{}) { []interface{}{"Example log message: %s", "example arg"} })
 	//var cont chan string
 	cont := make(chan string)
-	for _, url :=range inputString {
-		if(len(url)==0){break;}
-		
+	for _, url := range inputString {
+		if len(url) == 0 {
+			break
+		}
+
 		//log.Debug("url=%s",url)
 		go fetch(url, cont)
 	}
-	for i:=0;i<3;i++ {
-        input := <-cont
-        fmt.Printf("%s \n", input)
-    }
+	for range inputString {
+		input := <-cont
+		fmt.Printf("%s \n", input)
+	}
 	//fmt.Printf("%s", robots)
 	fmt.Println("Shutting Down")
 }
-func fetch (url string, cont chan string) ( err error ) {
-	
-	fmt.Printf("url=%s\n",url)
+func fetch(url string, cont chan string) (err error) {
+
+	fmt.Printf("url=%s\n", url)
 	//url = "http://www.google.com/robots.txt"
-	resp, err := http.Get(url)
+	resp, err := http.Get(strings.Trim(url, "\015\012"))
 	if err != nil {
 		log.Fatal("error message: %s", err)
 		return err
@@ -87,5 +90,5 @@ func fetch (url string, cont chan string) ( err error ) {
 		return err
 	}
 	return nil
-	
+
 }
